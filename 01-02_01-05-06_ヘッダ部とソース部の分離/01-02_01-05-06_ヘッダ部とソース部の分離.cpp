5472b42e59790d6e01a86efdbe9d20560bddcfe1
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-13 17:41:38 +0900

    01-02_01-05-06_ヘッダ部とソース部の分離
    ファイルのみ

---
+++ 01-02_01-05-06_ヘッダ部とソース部の分離/01-02_01-05-06_ヘッダ部とソース部の分離.cpp
@@ -0,0 +1,27 @@
+﻿//会計クラス(ソース部)
+
+#include<string>
+#include<iostream>
+
+#include "Accounting.h"
+
+using namespace std;
+
+//コンストラクタ
+Accounting::Accounting(string name, long amnt)
+{
+	full_name = name;
+	crnt_asset = amnt;
+}
+
+//収入がある
+void Accounting::earn(long amnt)
+{
+	crnt_asset += amnt;
+}
+
+//支出がある
+void Accounting::spend(long amnt)
+{
+	crnt_asset -= amnt;
+}
---
+++ 01-02_01-05-06_ヘッダ部とソース部の分離/Accounting.h
@@ -0,0 +1,17 @@
+//会計クラス（ヘッダファイル）
+
+#include<string>
+
+class Accounting {
+	std::string full_name;
+	long crnt_asset;
+
+public:
+	Accounting(std::string name, long amnt);
+
+	std::string name() { return full_name; }
+	long asset() { return crnt_asset; }
+
+	void earn(long amnt);
+	void spend(long amnt);
+};
