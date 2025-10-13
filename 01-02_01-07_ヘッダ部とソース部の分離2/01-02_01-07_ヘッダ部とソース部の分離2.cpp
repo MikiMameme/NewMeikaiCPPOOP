2b52dfde597da11cb520f4e349399a28ff3e2fd0
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-13 17:51:07 +0900

    01-02_01-07_ヘッダ部とソース部の分離2

---
+++ 01-02_01-07_ヘッダ部とソース部の分離2/01-02_01-07_ヘッダ部とソース部の分離2.cpp
@@ -0,0 +1,25 @@
+﻿//会計クラスの利用例
+
+#include<string>
+#include<iostream>
+#include"Accounting.h"
+
+using namespace std;
+
+//会計情報を表示
+void print_Accounting(string title, Accounting* p)
+{
+	cout << title << "\"" << p->name() << "\"" << p->asset() << "円\n";
+}
+
+int main()
+{
+	Accounting nakano("中野隼人", 1000);
+	Accounting yamada("山田宏文", 200);
+
+	nakano.spend(200);
+	yamada.earn(100);
+
+	print_Accounting("■中野:", &nakano);
+	print_Accounting("■山田:", &yamada);
+}
