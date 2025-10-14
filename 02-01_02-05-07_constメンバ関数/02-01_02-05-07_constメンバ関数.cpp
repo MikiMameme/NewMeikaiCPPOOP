74b52c95f0bf3e20a88b3342090a7525653402a6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-14 16:09:52 +0900

    02-01_02-05-07_constメンバ関数

---
+++ 02-01_02-05-07_constメンバ関数/02-01_02-05-07_constメンバ関数.cpp
@@ -0,0 +1,15 @@
+﻿//日付クラスの利用例（その２）
+
+#include<iostream>
+#include"Date.h"
+
+using namespace std;
+
+int main()
+{
+	Date today;
+
+	cout << "今日は" << today << "です\n";
+	cout << "昨日は" << today.preceding_day() << "です\n";
+	cout << "一昨日は" << today.preceding_day().preceding_day() << "です\n";
+}
