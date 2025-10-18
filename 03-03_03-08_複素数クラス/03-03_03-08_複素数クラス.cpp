5a2a8df2ec12782bbef04d5bfbd3ff63b4689315
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-18 21:17:35 +0900

    03-03_03-08_複素数クラス

---
+++ 03-03_03-08_複素数クラス/03-03_03-08_複素数クラス.cpp
@@ -0,0 +1,18 @@
+﻿//参照オブジェクトの参照先を検証
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double d = 1.0;
+	const int& p = 5;
+	const int& q = d;
+
+	const_cast<int&>(q) = 3.14;
+
+	cout << "d=" << d << '\n';
+	cout << "p=" << p << '\n';
+	cout << "q=" << q << '\n';
+}
