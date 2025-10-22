9f70718aa0e783b0253f25db563819dd835686b1
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-22 19:08:34 +0900

    04-02_04-07_例外の送出

---
+++ 04-02_04-07_例外の送出/04-02_04-07_例外の送出.cpp
@@ -0,0 +1,38 @@
+﻿//例外の送出と補足
+
+#include<iostream>
+#include<new>
+
+using namespace std;
+
+class OverFlow{ };
+
+int f(int x)
+{
+	if (x < 0)
+		throw"おかしいです、値が負になっています\n";
+	else if (x > 30000)
+		throw OverFlow();
+	else
+		return 2 * x;
+}
+
+int main()
+{
+	int a;
+	cout << "整数:";
+	cin >> a;
+
+	try {
+		int b = f(a);
+		cout << "その数の２倍は" << b << "です\n";
+	}
+	catch (const char* str) {
+		cout << "例外発生:" << str;
+	}
+
+	catch (OverFlow) {
+		cout << "オーバーフローしました、プログラムを終了します\n";
+		return 1;
+	}
+}
