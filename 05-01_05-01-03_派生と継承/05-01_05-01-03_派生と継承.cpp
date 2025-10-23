0d8fb6c71e367c662ea130c3f31e5e3a157cc14b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-23 20:42:50 +0900

    05-01_05-01-03_派生と継承

---
+++ 05-01_05-01-03_派生と継承/05-01_05-01-03_派生と継承.cpp
@@ -0,0 +1,18 @@
+﻿//一般会員クラスの利用例（その１）
+
+#include<iostream>
+#include"Member.h"
+
+using namespace std;
+
+int main()
+{
+	Member kiritan("東北きりたん", 15, 36.5);
+
+	double weight = kiritan.get_weight();
+	kiritan.set_weight(weight - 0.2);
+
+	cout << "会員番号" << kiritan.no() << "の" << kiritan.name()
+		<< "は" << kiritan.get_weight() << "kgです\n";
+
+}
