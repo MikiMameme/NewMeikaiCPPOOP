daf1af59c422bcc495ff45acebfae8eba8646fd3
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-24 16:55:11 +0900

    05-01_05-08-11_派生の形態

---
+++ 05-01_05-08-11_派生の形態/05-01_05-08-11_派生の形態.cpp
@@ -0,0 +1,20 @@
+﻿//private派生とメンバのアクセス性（注：コンパイルエラーになる場合あり）
+
+#include"Super.h"
+
+class Sub :private Super {
+	void f() {
+		//pri = 1; //クラス内部からもアクセスできないためコメント化
+		pro = 1;
+		pub = 1;
+	}
+};
+
+int main()
+{
+	Sub x;
+
+	//x.pri = 1; //クラス外部からは
+	//x.pro = 1; //アクセスできないため
+	//x.pub = 1; //コメント化している
+}
---
+++ 05-01_05-08-11_派生の形態/Super.h
@@ -0,0 +1,13 @@
+//Šî’êƒNƒ‰ƒXSuper
+#pragma once
+
+class Super {
+private:
+	int ori;
+
+protected:
+	int pro;
+
+public:
+	int pub;
+};
