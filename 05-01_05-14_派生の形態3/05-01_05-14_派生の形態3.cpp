f4dbfaf4daa9cf7c3ee6d8cea1836b4ce951e1f6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-25 12:59:41 +0900

    05-01_05-14_派生の形態3

---
+++ 05-01_05-14_派生の形態3/05-01_05-14_派生の形態3.cpp
@@ -0,0 +1,22 @@
+﻿//スライシング
+
+#include<iostream>
+#include"BaseDerived.h"
+
+using namespace std;
+
+int main()
+{
+	Base bs(99, 99);
+
+	cout << "BSの初期状態\n";
+	bs.func();
+
+	Derived dv(1, 2, 3);
+
+	bs = dv;
+	cout << "dvを代入した後\n";
+	bs.func();
+
+	//dv = bs;  //これは代入不可のためコメント化している
+}
