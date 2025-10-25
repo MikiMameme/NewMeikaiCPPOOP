3d669f4bfdb596c157d8a5b5625bae183622abc6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-25 14:11:53 +0900

    05-01_05-15_継承とデフォルトコンストラクタ

---
+++ 05-01_05-15_継承とデフォルトコンストラクタ/05-01_05-15_継承とデフォルトコンストラクタ.cpp
@@ -0,0 +1,30 @@
+﻿//基底クラスと派生クラスのコンストラクタ
+
+#include<iostream>
+
+using namespace std;
+
+//基底クラス
+class Base {
+	int x;
+
+public:
+	//コンストラクタ
+	Base() :x(99) { cout << "Base;;xを99で初期化\n"; }
+
+	//xのゲッタ
+	int get_x() const { return x; }
+};
+
+//派生クラス
+class Derived :public Base {
+	//コンストラクタを含め何も定義しない
+	//コンストラクタが存在しないにもかかわらずx=99でで初期化される
+};
+
+int main()
+{
+	Derived d;
+
+	cout << "d.get_x()=" << d.get_x() << '\n';
+}
