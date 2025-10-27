2c7c32f3ede38ff9574e6923c624bbe1353aa2f7
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-27 18:55:41 +0900

    05-01_05-18-19_継承と差分プログラミング

---
+++ 05-01_05-18-19_継承と差分プログラミング/05-01_05-18-19_継承と差分プログラミング.cpp
@@ -0,0 +1,18 @@
+﻿//優待会員クラスの利用例
+
+#include<iostream>
+#include"VipMember.h"
+
+using namespace std;
+
+int main()
+{
+	VipMember zunko("東北ずん子", 17, 52.5, "会費全額免除");
+
+	double weight = zunko.get_weight();
+	zunko.set_weight(weight + 0.5);
+
+	cout << "会員番号" << zunko.no() << "の" << zunko.name()
+		<< "は" << zunko.get_weight() << "kgで"
+		<< "特典は" << zunko.get_privilege() << "です\n";
+}
