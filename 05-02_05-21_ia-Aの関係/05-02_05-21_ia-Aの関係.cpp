af4b767223370ac846ca7bd24d7dd670bf3aaa61
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-28 18:19:44 +0900

    05-02_05-21_ia-Aの関係

---
+++ 05-02_05-21_ia-Aの関係/05-02_05-21_ia-Aの関係.cpp
@@ -0,0 +1,30 @@
+﻿//一般会員クラスと優待会員クラスの利用例
+
+#include<iostream>
+#include"Member.h"
+#include"VipMember.h"
+
+using namespace std;
+
+void slim_off(Member& m, double dw)
+{
+	double weight = m.get_weight();
+	if (weight > dw)
+		m.set_weight(weight - dw);
+}
+
+int main()
+{
+	Member kiritan("東北きりたん", 15, 40.3);
+	slim_off(kiritan, 0.3);
+
+	VipMember zunko("東北ずん子", 17, 52.5, "会費全額免除");
+	slim_off(zunko, 0.5);
+
+	cout << "No." << kiritan.no() << ":" << kiritan.name()
+		<< "(" << kiritan.get_weight() << "kg)\n";
+
+	cout << "No." << zunko.no() << ":" << zunko.name()
+		<< "(" << zunko.get_weight() << "kg)"
+		<< "特典=" << zunko.get_privilege() << '\n';
+}
