b8f3429e794cf91764067463da4c51c6db688909
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-29 17:44:13 +0900

    06-01_06-04_仮想関数と多相性2

---
+++ 06-01_06-04_仮想関数と多相性2/06-01_06-04_仮想関数と多相性2.cpp
@@ -0,0 +1,16 @@
+﻿//会員情報表示（優待会員と長寿会員に対して一般会員のprintを呼び出す）
+
+#include<iostream>
+#include"VipMember.h"
+#include"SeniorMember.h"
+
+using namespace std;
+
+int main()
+{
+	VipMember taro("田中太郎", 17, 89.2, "会費全額免除");
+	SeniorMember hanako("鈴木花子", 43, 63.7, 3);
+
+	taro.Member::print();
+	hanako.Member::print();
+}
