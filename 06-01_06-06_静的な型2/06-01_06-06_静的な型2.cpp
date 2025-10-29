eb09db7bbdcfd74898c9e9cd58f956b33cd9c25a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-29 18:15:32 +0900

    06-01_06-06_静的な型2

---
+++ 06-01_06-06_静的な型2/06-01_06-06_静的な型2.cpp
@@ -0,0 +1,20 @@
+﻿//基底クラスへのポインタ・参照が指す・参照するオブジェクトの型
+
+#include<typeinfo>
+#include<iostream>
+#include"Member.h"
+#include"VipMember.h"
+#include"SeniorMember.h"
+
+using namespace std;
+
+int main()
+{
+	VipMember taro("田中太郎", 17, 89.2, "会費全額免除");
+
+	Member* ptr = &taro;
+	Member& ref = taro;
+
+	cout << "ptrが指す先は" << typeid(*ptr).name() << "型オブジェクトです\n";
+	cout << "refの参照先は" << typeid(ref).name() << "型のオブジェクトです\n";
+}
