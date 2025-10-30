21ef61f39734420ea7dc63224116606217ae4c50
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-30 18:20:36 +0900

    06-01_06-10-11_仮想デストラクタ

---
+++ 06-01_06-10-11_仮想デストラクタ/06-01_06-10-11_仮想デストラクタ.cpp
@@ -0,0 +1,27 @@
+﻿//継承とデストラクタ
+
+#include<iostream>
+
+using namespace std;
+
+//基底クラス
+class Base {
+public:
+	Base() { cout << "Baseのコンストラクタ\n"; }
+	virtual ~Base() { cout << "Baseのデストラクタ\n"; }
+
+};
+
+//派生クラス
+class Derived :public Base {
+	int* a;
+public:
+	Derived() { a = new int[10]; cout << "配列を生成\n"; }
+	~Derived() { delete[] a; cout << "配列を解放\n"; }
+};
+
+int main()
+{
+	Base* ptr = new Derived;
+	delete ptr;
+}
