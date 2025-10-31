dc8a1cc3684103b1d0e8dbc667216ccb9b42248f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-31 16:00:55 +0900

    06-02_06-13_動的キャスト

---
+++ 06-02_06-13_動的キャスト/06-02_06-13_動的キャスト.cpp
@@ -0,0 +1,43 @@
+﻿//動的キャストと失敗時の挙動
+
+#include<iostream>
+#include<typeinfo>
+
+using namespace std;
+
+//基底クラス
+class Base {
+	virtual void f() {}
+};
+
+
+//派生クラス　
+class Derived :public Base {};
+
+int main()
+{
+	Base bs;
+	Derived dv;
+
+	Derived* p1 = dynamic_cast<Derived*>(&bs);
+	cout << "p1=" << p1 << '\n';
+
+	Derived* p2 = dynamic_cast<Derived*>(&dv);
+	cout << "p2=" << p2 << '\n';
+
+	try {
+		Derived& r1 = dynamic_cast<Derived&>(bs);
+		cout << "r1はbaを参照しています\n";
+	}
+	catch (bad_cast) {
+		cout << "bsのキャストに失敗しました\n";
+	}
+
+	try {
+		Derived& r2 = dynamic_cast<Derived&>(dv);
+		cout << "r2はdvを参照しています\n";
+	}
+	catch (bad_cast) {
+		cout << "dvのキャストに失敗しました\n";
+	}
+}
