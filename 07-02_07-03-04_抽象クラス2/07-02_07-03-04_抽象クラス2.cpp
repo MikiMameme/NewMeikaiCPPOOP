1abbb6ead5031f9888ec6a27885ba1bb27229c98
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-10-31 19:15:08 +0900

    07-02_07-03-04_抽象クラス2

---
+++ 07-02_07-03-04_抽象クラス2/07-02_07-03-04_抽象クラス2.cpp
@@ -0,0 +1,25 @@
+﻿//図形クラス群（その２）の利用例
+
+#include<iostream>
+#include"Shape.h"
+
+using namespace std;
+
+int main()
+{
+	Shape* a[] = {
+		new Point(),
+		new HorzLine(9),
+		new VertLine(6),
+		new Rectangle(7,3),
+	};
+
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
+		cout << "a[" << i << "]\n";
+		a[i]->print();
+		a[i]->debug();
+		cout << '\n';
+	}
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+		delete a[i];
+}
