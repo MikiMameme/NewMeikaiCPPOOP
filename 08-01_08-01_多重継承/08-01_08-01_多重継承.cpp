60b49563d195f08f622244ad279ed42c093c3c9f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-02 17:05:29 +0900

    08-01_08-01_多重継承

---
+++ 08-01_08-01_多重継承/08-01_08-01_多重継承.cpp
@@ -0,0 +1,48 @@
+﻿//多重継承（基底クラスの初期化・曖昧さの制御を検証）
+
+#include<iostream>
+
+using namespace std;
+
+//基底クラス
+class Base1 {
+public:
+	int x;
+
+	Base1(int a = 0) :x(a) {
+		cout << "Base1::xを" << x << "に初期化しました\n";
+	}
+	void print() { cout << "Base1クラスです:x=" << x << '\n'; }
+};
+
+//基底クラス２
+class Base2 {
+public:
+	int x;
+	Base2(int a = 0) :x(a) {
+		cout << "Base2::xを" << x << "に初期化しました\n";
+	}
+	void print() { cout << "Base2クラスです:x=" << x << '\n'; }
+};
+
+//派生クラス
+class Derived :public Base1, public Base2 {
+	int y;
+public:
+	Derived(int a, int b, int c) :y(c), Base2(a), Base1(b) {
+		cout << "Derived::yを" << y << "に初期化しました\n";
+	}
+	void func(int a, int b) {
+		Base1::x = a;
+		Base2::x = b;
+	}
+};
+
+int main()
+{
+	Derived z(1, 2, 3);
+
+	z.func(1, 2);
+	z.Base1::print();
+	z.Base2::print();
+}
