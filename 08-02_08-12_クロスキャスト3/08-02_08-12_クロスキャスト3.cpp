e83fda6a5ee1f85b0008de28938beaf868d4183c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-03 19:17:10 +0900

    08-02_08-12_クロスキャスト3

---
+++ 08-02_08-12_クロスキャスト3/08-02_08-12_クロスキャスト3.cpp
@@ -0,0 +1,29 @@
+﻿//図形クラス群の利用例（TwoDimensionalとしての多相性、クロスキャスト）
+
+#include<iostream>
+#include"Shape.h"
+#include"TwoDimensional.h"
+#include"Rectangle.h"
+#include"RectEquilTriangle.h"
+
+using namespace std;
+
+int main()
+{
+	TwoDimensional* t[] = {
+		new Rectangle(7,3),
+		new RectEquilTriangleLB(5),
+		new RectEquilTriangleRU(4),
+	};
+
+	for (int i = 0; i < sizeof(t) / sizeof(t[0]); i++) {
+		cout << "t[" << i << "]の面積は" << t[i]->get_area() << "です\n";
+
+		if (Shape* s = dynamic_cast<Shape*>(t[i]))
+			s->draw();
+		cout << '\n';
+	}
+
+	for (int i = 0; i < sizeof(t) / sizeof(t[0]); i++)
+		delete t[i];
+}
