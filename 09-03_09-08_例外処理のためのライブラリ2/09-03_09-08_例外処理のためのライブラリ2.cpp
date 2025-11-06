ab52455b6194454fc9aec90d4a6cedbb903fcf25
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-06 20:13:00 +0900

    09-03_09-08_例外処理のためのライブラリ2

---
+++ 09-03_09-08_例外処理のためのライブラリ2/09-03_09-08_例外処理のためのライブラリ2.cpp
@@ -0,0 +1,21 @@
+﻿//new演算子による記憶域の確保失敗時の挙動を変える
+
+#include<iostream>
+#include<cstdlib>
+#include<new>
+
+using namespace std;
+
+void new_error()
+{
+	cout << "new演算子による記憶域の確保に失敗しました\n";
+	abort();
+}
+
+int main()
+{
+	set_new_handler(new_error);
+
+	while (true)		//恣意的に例外を発生されるはずなのだが、
+		new char[10000]; //例外発生まで動く保証無し。このコードは推奨されないと思われる
+}
