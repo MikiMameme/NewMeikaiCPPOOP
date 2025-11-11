9080d5a80ff270cbf4afaec5b4caea7f56b1331a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-11 20:04:46 +0900

    11-02_11-12_ポインタと反復子

---
+++ 11-02_11-12_ポインタと反復子/11-02_11-12_ポインタと反復子.cpp
@@ -0,0 +1,20 @@
+﻿//ポインタによる配列要素の走査
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[5];
+	int value = 0;
+
+	//先頭から末尾へと走査して値を代入
+	for (int* p = &a[0]; p != &a[5]; p++)
+		*p = ++value;
+
+	//先頭から末尾へと走査して値を表示
+	for (const int* p = &a[0]; p != &a[5]; p++)
+		cout << *p << ' ';
+	cout << '\n';
+}
