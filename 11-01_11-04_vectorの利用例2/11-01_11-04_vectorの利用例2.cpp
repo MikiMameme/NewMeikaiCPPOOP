08993401b04b3cc0a1d1b88c906ec13d3d0848b5
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-11 18:53:18 +0900

    11-01_11-04_vectorの利用例2

---
+++ 11-01_11-04_vectorの利用例2/11-01_11-04_vectorの利用例2.cpp
@@ -0,0 +1,28 @@
+﻿//ベクトルの利用例
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int>x;
+
+	cout << "整数を入力せよ\n※終了は9999\n";
+
+	while (true) {
+		int temp;
+		cin >> temp;
+		if (temp==9999)break;
+		x.push_back(temp);
+	}
+
+	cout << "先頭データは" << x.front() << "です\n";
+	cout << "末尾から逆順に1個ずつ取り出して空にします\n";
+	while (x.size()) {
+		cout << x.back() << " ";
+		x.pop_back();
+	}
+	cout << '\n';
+}
