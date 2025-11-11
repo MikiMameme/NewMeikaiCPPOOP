11221540f24b6b3e14f29692368842e12a990ebe
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-11 17:48:53 +0900

    11-01_11-01_vectorの利用例

---
+++ 11-01_11-01_vectorの利用例/11-01_11-01_vectorの利用例.cpp
@@ -0,0 +1,23 @@
+﻿//vectorの利用例
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> x;
+
+	cout << "整数を入力せよ\n※終了は9999\n";
+
+	while (true) {
+		int temp;
+		cin >> temp;
+		if (temp == 9999)break;
+		x.push_back(temp);
+	}
+
+	for (vector<int>::size_type i = 0; i < x.size(); i++)
+		cout << "x[" << i << "]=" << x[i] << '\n';
+}
