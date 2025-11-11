7afd193b9e7d9250f6a2cc0d12aa8d08b6dc7ef4
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-11 21:00:24 +0900

    11-02_11-17_反復子を受け取る関数テンプレート

---
+++ 11-02_11-17_反復子を受け取る関数テンプレート/11-02_11-17_反復子を受け取る関数テンプレート.cpp
@@ -0,0 +1,28 @@
+﻿//ベクトルと配列の全要素の表示（反復子）
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//first,lastの全要素を{}で囲んで順に表示
+template<class InputIterator>
+void print(InputIterator first, InputIterator last)
+{
+	cout << "{";
+	for (InputIterator i = first; i != last; i++)
+		cout << *i << ' ';
+	cout << "}";
+}
+
+int main()
+{
+	int a[] = { 1,2,3,4,5 };
+	vector<int>x(a, a + 5);
+
+	cout << "a="; print(a, a + 5); cout << '\n';
+
+	cout << "x="; print(x.begin(), x.end()); cout << '\n';
+
+	cout << "x="; print(x.rbegin(), x.rend()); cout << '\n';
+}
