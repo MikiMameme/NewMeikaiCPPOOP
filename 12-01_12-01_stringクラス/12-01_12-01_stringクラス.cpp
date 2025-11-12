c6b6dd06b53703bfd39d55ec427e3511518902bb
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-12 18:44:20 +0900

    12-01_12-01_stringクラス

---
+++ 12-01_12-01_stringクラス/12-01_12-01_stringクラス.cpp
@@ -0,0 +1,13 @@
+﻿//stringとwstringの型名を表示
+
+#include<string>
+#include<typeinfo>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "typeid(string).name()=" << typeid(string).name() << "\n\n";
+	cout << "typeid(wstring).name()=" << typeid(wstring).name() << '\n';
+}
