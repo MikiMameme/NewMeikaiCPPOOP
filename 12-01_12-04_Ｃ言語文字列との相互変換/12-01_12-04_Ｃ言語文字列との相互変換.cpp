f902edc727f815782e62ff31e3e9f16c6e8d44bd
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-12 19:14:51 +0900

    12-01_12-04_Ｃ言語文字列との相互変換

---
+++ 12-01_12-04_Ｃ言語文字列との相互変換/12-01_12-04_Ｃ言語文字列との相互変換.cpp
@@ -0,0 +1,23 @@
+﻿//stringをＣ言語文字列に変換
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<string>
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char cst[100];
+	string str;
+
+	cout << "文字列を入力せよ:";
+	cin >> str;
+
+	strcpy(cst, str.c_str());
+
+	cout << "cst=" << cst << '\n';
+	cout << "str=" << str << '\n';
+}
