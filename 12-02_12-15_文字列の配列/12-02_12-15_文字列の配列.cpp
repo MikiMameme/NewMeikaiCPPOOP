f6a346ee1b6ff825bfe38e695eb6abb7b458c144
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-13 17:32:07 +0900

    12-02_12-15_文字列の配列

---
+++ 12-02_12-15_文字列の配列/12-02_12-15_文字列の配列.cpp
@@ -0,0 +1,39 @@
+﻿//文字列のベクトル
+
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+//文字列のベクトル1文字ずつ走査して表示
+template<class Allocator>
+void put_string_vector(const vector<string, Allocator>& v)
+{
+	cout << "{";
+	for (typename vector<string, Allocator>::size_type i = 0; i < v.size(); i++) {
+		cout << '"';
+		for (typename vector<string, Allocator>::size_type j = 0;
+			j < v[i].length(); j++)
+			cout << v[i][j];
+		cout << "\" ";
+	}
+	cout << "}";
+}
+
+int main()
+{
+	vector<string>s1(3);
+	vector<string>s2;
+	s2.push_back("ABC");
+	s2.push_back("123");
+	s2.push_back("XYZ");
+
+	cout << "s1=";
+	put_string_vector(s1);
+	cout << '\n';
+
+	cout << "s2=";
+	put_string_vector(s2);
+	cout << '\n';
+}
