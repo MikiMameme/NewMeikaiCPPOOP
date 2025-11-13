32596446bfea770bf23af5311d578bbc00b88993
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-13 18:37:07 +0900

    13-01_13-01_リダイレクト

---
+++ 13-01_13-01_リダイレクト/13-01_13-01_リダイレクト.cpp
@@ -0,0 +1,22 @@
+﻿//2つの整数値を読み込んで加減乗除した値を表示
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+
+	cout << "xとyを加減乗除します\n";
+
+	cout << "xとyの値:";
+	cin >> x >> y;
+
+	cout << "x + yは" << x + y << "です\n";
+	cout << "x - yは" << x - y << "です\n";
+	cout << "x * yは" << x * y << "です\n";
+	cout << "x / yは" << x / y << "です\n";
+	cout << "x % yは" << x % y << "です\n";
+}
