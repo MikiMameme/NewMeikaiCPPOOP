f13ab810fd1299a7c3ddf6efbb7916ebd4b0a161
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-14 17:48:49 +0900

    13-02_13-03_ファイルストリームに対する読み書き

---
+++ 13-02_13-03_ファイルストリームに対する読み書き/13-02_13-03_ファイルストリームに対する読み書き.cpp
@@ -0,0 +1,18 @@
+﻿//出力ファイルストリームへの書き込み
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream fos("HELLO");
+
+	if (!fos)
+		cerr << "\aファイル\"HELLO\"をオープンできません\n";
+	else {
+		fos << "Hello!\n";
+		fos << "How are you?\n";
+	}
+}
