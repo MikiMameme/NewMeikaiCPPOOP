1c569afeea4035be8a3d55529590f67196abd045
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-14 18:51:35 +0900

    13-03_13-11_ストリームライブラリ2

---
+++ 13-03_13-11_ストリームライブラリ2/13-03_13-11_ストリームライブラリ2.cpp
@@ -0,0 +1,36 @@
+﻿//フラグの設定と復元
+
+#include<iostream>
+
+using namespace std;
+
+void put_ary(double ary[], int n)
+{
+	ios_base::fmtflags flags = ios_base::right | ios_base::dec | ios_base::fixed;
+
+	ios_base::fmtflags old_flags = cout.flags();
+	streamsize old_size = cout.width();
+	streamsize old_prec = cout.precision(2);
+
+	for (int i = 0; i < n; i++) {
+		cout.width(10);
+		cout.flags(flags);
+		cout << ary[i] << '\n';
+	}
+
+	cout.flags(old_flags);
+	cout.width(old_size);
+	cout.precision(old_prec);
+}
+
+int main()
+{
+	double a[] = { 1234.235,5568.6205,78999.312 };
+
+	cout << 0.00001234567890 << "\n\n";
+
+	put_ary(a, sizeof(a) / sizeof(a[0]));
+	cout << '\n';
+
+	cout << 0.00001234567890 << '\n';
+}
