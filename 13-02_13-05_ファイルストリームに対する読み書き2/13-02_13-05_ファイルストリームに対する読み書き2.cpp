c72389810e7187200feb615db96c9e898b89d75f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-14 18:16:55 +0900

    13-02_13-05_ファイルストリームに対する読み書き2

---
+++ 13-02_13-05_ファイルストリームに対する読み書き2/13-02_13-05_ファイルストリームに対する読み書き2.cpp
@@ -0,0 +1,23 @@
+﻿//入力ファイルストリームから文字列を行単位に読み込んで表示
+
+#include<string>
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ifstream fis("HELLO");
+
+	if (!fis)
+		cerr << "\aファイル\"HELLO\"をオープンできません\n";
+	else {
+		while (true) {
+			string text;
+			getline(fis, text);
+			if (!fis)break;
+			cout << text << '\n';
+		}
+	}
+}
